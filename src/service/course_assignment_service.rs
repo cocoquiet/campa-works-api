618789782329079c93aa9a55010c1d38d74886d8
JiSeong Lee <cocoquiet@knu.ac.kr>
2026-09-21 12:51:52 +0900

    Feat: Implement CourseAssignmentService::create_auto_in_new_semester() without round-based algorithm

--- src/service/course_assignment_service.rs
+++ src/service/course_assignment_service.rs
@@ -7,6 +7,9 @@ use crate::{
     models::course_assignment::NewCourseAssignment,
     repository::{
         course_assignment_repository::CourseAssignmentRepository,
+        course_curriculum_repository::CourseCurriculumRepository,
+        course_pool_repository::CoursePoolRepository,
+        course_preference_repository::CoursePreferenceRepository,
         course_repository::CourseRepository, professor_repository::ProfessorRepository,
     },
 };
@@ -51,6 +54,76 @@ impl CourseAssignmentService {
             .unwrap_or_else(|| unreachable!());
 
         Ok(course_assignment.into())
+    }
+
+    pub fn create_auto_in_new_semester(
+        conn: &mut PgConnection,
+        semester_id: i64,
+    ) -> Result<Vec<CourseAssignmentResponse>, AppError> {
+        let courses = CourseRepository::find_all(
+            conn,
+            &HashMap::from([("semester_id".to_string(), semester_id.to_string())]),
+        )
+        .map_err(|_| AppError::DatabaseError)?;
+        let professors = ProfessorRepository::find_all(
+            conn,
+            &HashMap::from([("professor_status".to_string(), "ACTIVE".to_string())]),
+        )
+        .map_err(|_| AppError::DatabaseError)?;
+
+        // Init hungarian matrix(rows: professors, columns: courses, values: course_preference_score)
+        let mut hungarian_matrix = vec![vec![0; courses.len()]; professors.len()];
+        for (row_idx, (professor, _, _)) in professors.iter().enumerate() {
+            let professor_course_preferences = CoursePreferenceRepository::find_all(
+                conn,
+                &HashMap::from([("professor_id".to_string(), professor.id.to_string())]),
+            )
+            .map_err(|_| AppError::DatabaseError)?;
+
+            for (col_idx, (course, _, _, _, _, _)) in courses.iter().enumerate() {
+                let master_course_id =
+                    CourseCurriculumRepository::find_by_id(conn, course.course_curriculum_id)
+                        .map_err(|_| AppError::DatabaseError)?
+                        .1
+                        .id;
+
+                let course_preferencec_score_query_params = HashMap::from([
+                    ("professor_id".to_string(), professor.id.to_string()),
+                    ("semester_id".to_string(), semester_id.to_string()),
+                    ("master_course_id".to_string(), master_course_id.to_string()),
+                ]);
+                hungarian_matrix[row_idx][col_idx] = match CoursePreferenceRepository::find_all(
+                    conn,
+                    &course_preferencec_score_query_params,
+                )
+                .map_err(|_| AppError::DatabaseError)?
+                .into_iter()
+                .next()
+                {
+                    Some((course_preference, _, _, _, _)) => course_preference.priority,
+                    None => {
+                        if CoursePoolRepository::find_all(
+                            conn,
+                            &HashMap::from([
+                                ("professor_id".to_string(), professor.id.to_string()),
+                                ("master_course_id".to_string(), master_course_id.to_string()),
+                            ]),
+                        )
+                        .unwrap()
+                        .is_empty()
+                        {
+                            9
+                        } else {
+                            8
+                        }
+                    }
+                };
+            }
+        }
+
+        // ToDo: Implement Round-Based Hungarian algorithm
+
+        Ok(Vec::new())
     }
 
     pub fn get_all(
